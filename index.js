3419a9634f663768c3c3eebb157a0deab8678d52
Maria Cristina Ordonez <crisordonez44@gmail.com>
2022-09-27 16:15:46 -0500

    commit tree 1e33b6cc71221e91267792aba1f0ebe1b40ff208

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(names,event) {
+    let thankYouCards=[]
+    for (let i = 0; i <= writeCards.length; i++) {
+      thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    console.log (thankYouCards);
+    return  thankYouCards;
+  }
+
+  function countDown(startingNumber) {
+    while (startingNumber>0){
+        console.log (startingNumber);
+        //startingNumber = startingNumber -1;
+        startingNumber-=1
+
+    };
+    console.log (startingNumber);
+   
+  }
